737c7d90949b1e7cf383f020174b4273049f2ace
RujutaB <60830306+RujutaB@users.noreply.github.com>
2021-06-05 10:19:17 +0530

    frontend completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,23 @@ import {Container} from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom' //browser router uses html5 history api using push-state and replace-state
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>     {/*replace empty tag with Router tag or could have also created router as a new tag */}
       <Header />
       <main className='py-3'>
         <Container>
-        <HomeScreen />
+        <Route path ='/' component={HomeScreen} exact />
+        <Route path ='/product/:id' component={ProductScreen} />
         
         </Container>
       </main>
   
       <Footer />
-    </>
+    </Router>
   )
 }
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
+import {Link} from 'react-router-dom'
 
 const Product = ({product}) => {   //destructuring product array instead of props
     return (
         <Card className='my-3 p-3 rounded'>  {/*add margin and padding*/}
-            <a href= {`/product/${product._id}`}>
+            <Link to= {`/product/${product._id}`}>
                 <Card.Img src = {product.image} variant='top' /> {/*photo*/}
-            </a>
+            </Link>
             <Card.Body>
-                <a href= {`/product/${product._id}`}>    {/*title*/}
+                <Link to= {`/product/${product._id}`}>    {/*title*/}
                     <Card.Title as ='div'>
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </a> 
+                </Link> 
                 
                 <Card.Text as ='div'> {/*review*/}
                     <Rating  
